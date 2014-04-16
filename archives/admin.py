0fd3a2b344b3eb4a0358ddf93a2cc7213c2b2e17
callenrain <callenrain@gmail.com>
2014-04-16 15:47:25 -0400

    new db, display information for admin panel

--- archives/admin.py
+++ archives/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from archives.models import Presenter
 
-admin.site.register(Presenter)
+class PresenterAdmin(admin.ModelAdmin):
+	list_display = ('name', 'major', 'grad_year', 'project_title')
+	fieldsets = [
+        ('Personal',               {'fields': ['name', 'major', 'grad_year']}),
+        ('Project Information', {'fields': ['project_title', 'informal_abstract', 'formal_abstract']}),
+        ('External Data', {'fields': ['project_docs', 'project_image', 'video', 'profile_image']}),
+    ]
+
+admin.site.register(Presenter, PresenterAdmin)
--- archives/models.py
+++ archives/models.py
@@ -4,11 +4,14 @@ class Presenter(models.Model):
 	name = models.CharField(max_length=200)
 	major = models.CharField(max_length=200)
 	grad_year = models.IntegerField(default=0)
-	#project = models.ForeignKey(Project, blank=True, null=True)
+	project_title = models.CharField(max_length=200, blank=True, null=True)
 	profile_image = models.ImageField(upload_to="profile_images", blank=True, null=True)
 	formal_abstract = models.TextField(blank=True, null=True)
 	informal_abstract = models.TextField(blank=True, null=True)
 	video = models.URLField(blank=True, null=True)
 	project_image = models.ImageField(upload_to="project_images", blank=True, null=True)
 	project_docs = models.ImageField(upload_to="project_docs", blank=True, null=True)
+
+	def __unicode__(self):
+		return self.name
 
