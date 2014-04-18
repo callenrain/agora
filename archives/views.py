c54cb3907aa72474dd5052e791ae02534beb5ad0
Justin Cosentino <justintcosentino@gmail.com>
2014-04-18 15:45:51 -0400

    Updated index to account for new view

--- archives/views.py
+++ archives/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from archives.models import Presenter
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the archives index.")
+	archives_list = Presenter.objects.order_by('grad_year')
+	template = loader.get_template("archives/index.html")
+	context = RequestContext(request, {
+		"archives_list" : archives_list,
+		})
+	return HttpResponse(template.render(context))
 
 def grad_year(request, grad_year):
     return HttpResponse("You're looking at grad_year %s." % grad_year)
