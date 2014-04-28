ea132c624ebc54591eb18639981c3b398bed6dcd
callenrain <callenrain@gmail.com>
2014-04-28 14:07:37 -0400

    add nickname saving to model

--- archives/models.py
+++ archives/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Presenter(models.Model):
 	first_name = models.CharField(max_length=200,default="")
 	last_name = models.CharField(max_length=200,default="")
+	nickname = models.CharField(max_length=200, default="")
 	major = models.CharField(max_length=200,default="")
 	grad_year = models.IntegerField(default=0)
 	project_title = models.CharField(max_length=200, blank=True, null=True)
@@ -13,11 +14,13 @@ class Presenter(models.Model):
 	project_image = models.ImageField(upload_to="project_images", blank=True, null=True)
 	project_docs = models.ImageField(upload_to="project_docs", blank=True, null=True)
 
-	def display_name(self):
-		return "%s %s" % (self.first_name, self.last_name)
-
 	def url_name(self):
-		return "%s_%s" % (self.first_name.lower(), self.last_name.lower())
+		return "%s_%s" % (self.first_name.lower().strip(), self.last_name.lower().strip())
 
 	def __unicode__(self):
 		return "%s %s" % (self.first_name, self.last_name)
+
+	def save(self, *args, **kwargs):
+		if self.nickname == "":
+			self.nickname = self.url_name()
+			super(Presenter, self).save(*args, **kwargs)
