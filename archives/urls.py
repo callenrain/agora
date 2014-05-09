93f9c9ca3f7882809dd74c46168d96550588bc37
Justin Cosentino <justintcosentino@gmail.com>
2014-05-09 10:57:15 -0400

    urls

--- archives/urls.py
+++ archives/urls.py
@@ -3,6 +3,6 @@ from archives import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<grad_year>[0-9]+)/$', views.YearView.as_view(), name='year'),
-    url(r'^(?P<grad_year>[0-9]+)/(?P<presenter_id>\w+)/$', views.PresenterView.as_view(), name='presenter')
+    url(r'^(?P<pk>[0-9]+)/$', views.YearView.as_view(), name='year'),
+    url(r'^(?P<pk>[0-9]+)/(?P<presenter_id>\w+)/$', views.PresenterView.as_view(), name='presenter')
 )
